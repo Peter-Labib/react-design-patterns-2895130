d314162b6a26ee4ad7775a7fc6feed98afdb2678
Peter Labib <peter.adel.labib@gmail.com>
2023-06-28 00:52:35 +0300

    layout component * split screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { SplitScreen } from './SplitScreen';
 
 const LeftHandComponent = () => {
-	return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
-}
+  return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+};
 
 const RightHandComponent = () => {
-	return <p style={{ backgroundColor: 'red' }}>Right!</p>;
-}
+  return <p style={{ backgroundColor: 'red' }}>Right!</p>;
+};
 
 function App() {
-	return (
-		<SplitScreen
-			left={LeftHandComponent}
-			right={RightHandComponent} />
-	);
+  return (
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent />
+      <RightHandComponent />
+    </SplitScreen>
+  );
 }
 
 export default App;
--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -1,25 +1,18 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-	display: flex;
+  display: flex;
 `;
-
 const Pane = styled.div`
-	flex: 1;
+  flex: ${(props) => props.weight};
 `;
 
-export const SplitScreen = ({
-	left: Left,
-	right: Right,
-}) => {
-	return (
-		<Container>
-			<Pane>
-				<Left />
-			</Pane>
-			<Pane>
-				<Right />
-			</Pane>
-		</Container>
-	);
-}
+export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  const [left, right] = children;
+  return (
+    <Container>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
+    </Container>
+  );
+};
